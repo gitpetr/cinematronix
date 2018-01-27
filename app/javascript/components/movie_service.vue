2a61824f5a1c42399679a233dee95687452ff143
pkhilenko <pkhilenko@yandex.ru>
2018-01-27 09:02:06 +0600

    complete

---
+++ app/javascript/components/movie_service.vue
@@ -0,0 +1,14 @@
+<script>
+  export default {
+    props: ["resource"],
+    methods: {
+      fetch() {
+        this.$http.get(this.endpoint).then(response => {
+          this.movies = response.body
+        }, response => {
+          console.log('ошибка')
+        })
+      }
+    }
+  }
+</script>
