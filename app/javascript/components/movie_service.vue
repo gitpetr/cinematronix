8fcedb584b7c585eba05f6ea5ec1e84b688e639f
pkhilenko <pkhilenko@yandex.ru>
2018-01-29 13:48:23 +0600

    exit

--- app/javascript/components/movie_service.vue
+++ app/javascript/components/movie_service.vue
@@ -1,9 +1,12 @@
 <script>
   export default {
-    props: ["resource"],
+    data() {
+      endpoint: 'http://localhost:3000/api/v1/movies'
+    },
+    props: ["movies"],
     methods: {
       fetch() {
-        this.$http.get(this.endpoint).then(response => {
+        this.$http.get(endpoint).then(response => {
           this.movies = response.body
         }, response => {
           console.log('ошибка')
