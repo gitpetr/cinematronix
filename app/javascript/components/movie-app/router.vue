682348f0005aabf8708867b11711ff151dcb82e6
pkhilenko <pkhilenko@yandex.ru>
2018-02-01 19:25:08 +0600

    del row from router

--- app/javascript/components/movie-app/router.vue
+++ app/javascript/components/movie-app/router.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <div class='container'>
       <router-link :to="{ path: '/movies' }">All movies</router-link>
-      <div class="row">
        <router-view></router-view>
-      </div>
     </div>
   </div>
 </template>
