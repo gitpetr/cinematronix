c3275e41afe797c269762f429f07e40e422e7029
pkhilenko <pkhilenko@yandex.ru>
2018-02-09 18:14:51 +0600

    error booking-form

--- app/javascript/components/booking-app/booking-form.vue
+++ app/javascript/components/booking-app/booking-form.vue
@@ -1,13 +1,42 @@
 <template>
-
+  <div>
+    <form>
+      <input type="text" v-model="movie_session_id"/>
+      <label for="name"/>
+      <input type="text" v-model="name" id="name"><br/>
+      <label for="email"/>
+      <input type="email" v-model="email" id="email"><br/>
+      <label for="phone"/>
+      <input type="text" v-model="phone" id="phone"><br/>
+      <label for="row"/>
+      <input type="text" v-model="row" id="row"/>
+      <label for="char"/>
+      <input type="text" v-model="char" id="char"/><br/>
+      <input type="submit" value='ok'/>
+    </form>
+  </div>
 </template>
 
 <script>
   export default {
-    props: ['booking']
+    props: ["movie_session_id"],
+    data() {
+      return {
+        movie_session_id: movie_session_id,
+        name: '',
+        email: '',
+        phone: '',
+        row: '',
+        char: '',
+        seat: ''
+      }
+    },
+    watch: {
+      seat() {
+        this.seat = (this.char + ':' + this.char)
+      }
+    }
   }
 </script>
 
-<style scoped>
 
-</style>
--- app/javascript/components/booking-app/booking-new.vue
+++ app/javascript/components/booking-app/booking-new.vue
@@ -1,14 +1,17 @@
 <template>
   <div class='container'>
     <h3>Забронировать билет</h3>
-  <p>{{movie_session_id}}</p>
+    <p>{{movie_session_id}}</p>
+    <booking-form :movie_session_id="movie_session_id"></booking-form>
   </div>
 </template>
 
 <script>
+  import BookingForm from '../../components/booking-app/booking-form'
   import MovieService from '../../services/movies.service.js'
 
   export default {
+    components: {BookingForm},
     data() {
       return {
         booking: Object,
