3715d2e5a699384f6adcb8bb566856a2d306bbd4
pkhilenko <pkhilenko@yandex.ru>
2018-02-12 15:02:59 +0600

    add array bookingseats to createHall

--- app/javascript/services/movies.service.js
+++ app/javascript/services/movies.service.js
@@ -29,10 +29,9 @@ export default {
     const endpoint = `http://localhost:3000/api/v1/moviesession/${movie_session_id}`
     self.$http.get(endpoint).then(response => {
       let seats = response.body.bookings.map(function(booking) {
-        return booking.seat;
+        return seats;
       });
       self.bookingseats = seats
-      console.log(seats)
     }, response => {
       console.log('ошибка')
     })
@@ -59,6 +58,18 @@ export default {
     })
   },
   createHall(self) {
+    const bookseats = []
+    const movie_session_id = self.$route.params.movie_session_id
+    const endpoint = `http://localhost:3000/api/v1/moviesession/${movie_session_id}`
+    self.$http.get(endpoint).then(response => {
+      response.body.bookings.map(function(booking) {
+        bookseats.push(booking.seat)
+      });
+    }, response => {
+      console.log('ошибка')
+    });
+    console.log(bookseats)
+
     var Hall = {
           rows: [35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35]
         },
@@ -66,8 +77,7 @@ export default {
     $.each(Hall.rows, function(row, numberOfSeats) {
       var HallRow = '';
       for (var i = 1; i <= numberOfSeats; i++) {
-        HallRow += '<td class="' + (row + 1) + ':' + i +  ' seat">' +
-            i + '</td>';
+        HallRow += `<td class="${(row + 1)}:${i} seat">${i}</td>`
       }
       HallMap += HallRow + '<td class="num-row">'+ (row + 1) + 'ряд' +'</td>' + '</tr>';
     });
